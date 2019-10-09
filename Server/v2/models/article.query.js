a0c454524cf8ba8f7444b3081684a15423c64a57
Nuru <abdoulniyigena@gmail.com>
2019-10-09 22:27:13 +0200

    ft(endpoint): add modify article endpoint with database [Finishes #169039162]

--- Server/v2/models/article.query.js
+++ Server/v2/models/article.query.js
@@ -9,7 +9,17 @@ const createArticle = ` insert into articles (
 
 const findOneArticle = `select articleId, title, article, category, createdOn
                         from articles where articleid=($1)`;
+const findOneToUpdate = `select articleId, title, article, category, createdOn,
+                        createdBy from articles where articleid=($1)`;
 const findAuthor = `select employeeid, firstname, email, jobRole, department 
                     from employee where employeeid=($1)`;
+const updateArticle = `update articles set title=($1), article=($2) 
+                      where articleid=($3) RETURNING *`;
 
-export default { createArticle, findOneArticle, findAuthor };
+export default {
+  createArticle,
+  findOneArticle,
+  findAuthor,
+  updateArticle,
+  findOneToUpdate
+};
